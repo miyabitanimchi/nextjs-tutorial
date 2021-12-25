46f918347e2c94dfbf9b35f76b9350e04222a97a
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-12-24 23:42:34 -0800

    first commit

---
+++ components/layout.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from "./layout.module.css";
+
+const Layout = ({ children }) => (
+  <div className={styles.container}>{children}</div>
+)
+
+export default Layout;
